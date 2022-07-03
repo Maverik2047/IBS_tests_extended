adedd756d66523808d79a1d43f1a89f5789e65e4
Viacheslav <maverik2047@yandex.ru>
2022-07-04 00:57:08 +0300

    project added

---
+++ src/test/java/cloud/autotests/tests/TestBase.java
@@ -0,0 +1,40 @@
+package cloud.autotests.tests;
+
+import cloud.autotests.helpers.AllureAttachments;
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+
+public class TestBase extends AllureAttachments {
+    @BeforeAll
+    static void beforeAll() {
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        Configuration.browserCapabilities = capabilities;
+        capabilities.setCapability("enableVNC", true);
+        capabilities.setCapability("enableVideo", true);
+        Configuration.baseUrl = "https://ibs.ru";
+        Configuration.browserSize = "1800x900";
+        Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
+    }
+
+    @BeforeEach
+    public void beforeEach() {
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+    }
+
+    @AfterEach
+    public void afterEach() {
+
+        screenshotAs("Last screenshot");
+        pageSource();
+        browserConsoleLogs();
+        addVideo();
+
+
+    }
+}
